4b7a99c305bd2a23bdfdb96379ce658e5e8966c2
Ashley Pope <irashp@gmail.com>
2011-08-09 13:38:13 +1000

    Loads sections and reports errors for any not-loaded-sections.

--- lib/Simple/Quiz.pm
+++ lib/Simple/Quiz.pm
@@ -7,15 +7,32 @@ use Data::Dumper;
 
 has 'filename', is => 'rw', isa => 'Str';
 has 'title', is => 'rw', isa => 'Str';
-has 'sections', is => 'rw', isa => 'ArrayRef', predicate => '_has_sections';
+has 'sections', is => 'rw', isa => 'ArrayRef', predicate => '_has_sections', default => sub { [] };
 
 sub load_sections {
-  my ($self, @sections) = @_;
-
+  my ($self, $sections) = @_;
+  
   open my $fh, '<', $self->filename;
   my $questions_input = LoadFile($fh);
-  print Dumper($questions_input->{questions}{sections}{1});
-  print Dumper(@sections);
+
+  my @section_errors;
+
+  # Read through sections and load all found sections into sections.
+  foreach (@{$sections}) { 
+    my $section = $questions_input->{questions}{sections}{$_};
+     if (defined $section) {
+      push @{$self->sections}, $section;
+     } else {
+      push @section_errors, $_;
+     }
+  }
+
+  if (scalar @section_errors >= 1) {
+    say "Error can't load following sections: @section_errors";
+  }
+
+  #$questions_input->{questions}{sections}{1});
+  #print Dumper(@sections);
 
   $self->sections(["Load the sections here"]);
 }
