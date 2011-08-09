b905b13832d2057d7b74980b4a62a276e1ad9104
Ashley Pope <irashp@gmail.com>
2011-08-09 14:48:18 +1000

    fixed up indentation.

--- lib/Simple/Quiz.pm
+++ lib/Simple/Quiz.pm
@@ -20,19 +20,16 @@ sub load_sections {
   # Read through sections and load all found sections into sections.
   foreach (@{$sections}) { 
     my $section = $questions_input->{questions}{sections}{$_};
-     if (defined $section) {
+    if (defined $section) {
       push @{$self->sections}, $section;
-     } else {
+    } else {
       push @section_errors, $_;
-     }
+    }
   }
 
   if (scalar @section_errors >= 1) {
     say "Error can't load following sections: @section_errors";
   }
-
-  #$questions_input->{questions}{sections}{1});
-  #print Dumper(@sections);
 
   $self->sections(["Load the sections here"]);
 }
