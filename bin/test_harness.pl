06cf6535b23cd67b53d06e5cda328fb2a3086a24
Ashley Pope <irashp@gmail.com>
2011-08-09 13:13:27 +1000

    loading questions from questions.yaml works, specify filename and title to quiz at init

--- bin/test_harness.pl
+++ bin/test_harness.pl
@@ -5,12 +5,12 @@ use Simple::Quiz;
 
 my $quiz_file = $ARGV[0];
 
-my $quiz = Simple::Quiz->new(filename => $quiz_file);
+my $quiz = Simple::Quiz->new(filename => $quiz_file, title => "Learning cantonese");
 
 if ($quiz->load_sections(1,5)) {
   say "Sections loaded successfully";
 }
 
 if ($quiz->start()) {
-  say "Quiz started successfully";
+  say "Quiz started successfully: " . $quiz->title;
 }
--- lib/Simple/Quiz.pm
+++ lib/Simple/Quiz.pm
@@ -1,9 +1,12 @@
 package Simple::Quiz;
 
+use Modern::Perl;
 use Moose;
+use YAML::XS qw/LoadFile/;
 use Data::Dumper;
 
 has 'filename', is => 'rw', isa => 'Str';
+has 'title', is => 'rw', isa => 'Str';
 has 'section_start', is => 'rw', isa => 'Int';
 has 'section_max', is => 'rw', isa => 'Int';
 has 'sections', is => 'rw', isa => 'ArrayRef', predicate => '_has_sections';
@@ -12,6 +15,11 @@ sub load_sections {
   my ($self, $start, $max) = @_;
   $self->section_start($start);
   $self->section_max($max);
+
+  open my $fh, '<', $self->filename;
+  my $questions_input = LoadFile($fh);
+  print Dumper($questions_input->{questions}{sections}{1});
+
   $self->sections(["Load the sections here"]);
 }
 
@@ -21,7 +29,7 @@ sub start {
   if (!$self->_has_sections) {
     die("Error: No sections specified for quiz");
   }
-
+  
   return 1;
 }
 
