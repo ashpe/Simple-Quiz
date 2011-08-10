49dee3ad63c729b5ef660e6e4ec3a7fbc5557c8f
Ashley Pope <irashp@gmail.com>
2011-08-10 14:00:20 +1000

    ...got....pretty...commenty things.

--- lib/Simple/Quiz.pm
+++ lib/Simple/Quiz.pm
@@ -1,4 +1,7 @@
 package Simple::Quiz;
+{
+  $Simple::Quiz::VERSION = '0.001';
+}
 
 # ABSTRACT: Simple quiz API
 
@@ -135,3 +138,28 @@ sub section_complete {
 }
 
 1;
+
+__END__
+=pod
+
+=head1 NAME
+
+Simple::Quiz - Simple quiz API
+
+=head1 VERSION
+
+version 0.001
+
+=head1 AUTHOR
+
+Ashley Pope <irashp@gmail.com>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2011 by Ashley Pope.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
+=cut
+
