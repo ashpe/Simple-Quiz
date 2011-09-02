56ad197b6ba2f81bd30654b8b07aea4354d94e1f
Ashley Pope <irashp@gmail.com>
2011-09-02 18:28:21 +1000

    added dummy check formatting of each section.

--- lib/Simple/Quiz/Create.pm
+++ lib/Simple/Quiz/Create.pm
@@ -12,7 +12,7 @@ use Carp;
 #has 'completed_questions', is => 'rw', isa => 'ArrayRef', default => sub { [] };
 
 
-# Simple::Quiz::Create->(title => "Cake", sections => \%sections, questions => \@questions, filename => "super_quiz");
+# Simple::Quiz::Create->new(title => "Cake", sections => \%sections, filename => "super_quiz");
 # Simple::Quiz::Create->generate_quiz;
 
 has 'title', is => 'rw', isa => 'Str';
@@ -29,14 +29,22 @@ sub generate_quiz {
             croak "$attribute is required to generate quiz!";
         }
     }
+    
+    if (!$self->_check_section_format) {
+        croak "Sections are incorrect format, must be a hash of arrays which contain hashes, with question/answer in minimum in each array.";
+    }    
 
     my %quiz;
-
     $quiz{title} = $self->title if $self->title;
     #TODO: Add check that sections is correct format
     $quiz{questions} = $self->sections;
    
     DumpFile($self->filename, \%quiz);    
+}
+
+sub _check_section_format {
+    my $self = shift;
+    return 1;
 }
 
 sub add_section {
